9dd7293e4db99c6c4459e48370af68a338bfc685
BiyaMariamBiju <83966780+BiyaMariamBiju@users.noreply.github.com>
2021-05-12 15:02:14 +0000

    Create day2task2.c

---
+++ day2task2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+   float n,b;
+    scanf("%f",&b);
+        n=sqrt(b);
+        printf("%.1f",n);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
