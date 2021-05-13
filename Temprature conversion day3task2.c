fb89be2e3a8fd99342698fbaf02038c3069777d2
BiyaMariamBiju <83966780+BiyaMariamBiju@users.noreply.github.com>
2021-05-13 13:30:50 +0000

    Create Temprature conversion day3task2.c

---
+++ Temprature conversion day3task2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(){
+    
+    float T;
+    
+    scanf("%f",&T);
+        
+    printf("%.2f",(T-32)*5/9);
+}
