291c294254dd0634c093986c6a5acaa9031b31de
BiyaMariamBiju <83966780+BiyaMariamBiju@users.noreply.github.com>
2021-05-13 13:27:48 +0000

    Create Area of shapes day3task1.c

---
+++ Area of shapes day3task1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+     int a;
+     float r,b,h;
+    
+     scanf("%d%f%f%f",&a,&r,&b,&h);
+    
+     printf("%d\n%.1f\n%.2f\n",a*a,3.14*r*r,0.5*b*h);
+      
+  /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
