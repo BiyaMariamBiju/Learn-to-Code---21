de67de16d02bed30a69e369e75abc9362443c3e7
BiyaMariamBiju <83966780+BiyaMariamBiju@users.noreply.github.com>
2021-05-17 06:57:44 +0000

    Create Triangle day6task2.c

---
+++ Triangle day6task2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    int a,b,c,sum;
+    scanf("%d%d%d",&a,&b,&c);
+    sum=a+b;
+    if (sum>c)
+    {
+        printf("Yes");
+    
+    }
+    else
+    {
+        printf("No");
+    }
+        
+
+    
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
