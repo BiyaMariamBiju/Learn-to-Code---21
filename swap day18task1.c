67517828f17a1ec51da58ddaf0917afba48ec33f
BiyaMariamBiju <83966780+BiyaMariamBiju@users.noreply.github.com>
2021-06-04 18:09:49 +0000

    Update swap day18task1.c

--- swap day18task1.c
+++ swap day18task1.c
@@ -2,28 +2,29 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
-int numbers();
+
+
+int numbers(int,int);
 int main() 
-{
-    numbers();
-}
-int numbers()
-{
-    int a,b,temp;
-   
+{ 
+    int a,b;
     scanf("%d%d",&a,&b);
+    numbers(a,b);
+}
+int numbers(int a,int b)
+{
+    int temp;
     printf("a=%d",a);
-     printf(" ");
+    printf(" ");
     printf("b=%d",b);
     printf("\n");
+    
     temp=a;
     a=b;
     b=temp;
     printf("a=%d",a);
     printf(" ");
     printf("b=%d",b);
-    
-
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
     return 0;
 }
